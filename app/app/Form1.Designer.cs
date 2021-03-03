98190090dfb505cb06465b22ade6619f5869796e
KirameneSalen <benko.claudia@yahoo.com>
2021-03-03 08:32:03 +0200

    Lab1

---
+++ app/app/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.conectare = new System.Windows.Forms.Button();
+            this.powerTextBox = new System.Windows.Forms.TextBox();
+            this.power = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.effectTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.type = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(647, 291);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // conectare
+            // 
+            this.conectare.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conectare.Location = new System.Drawing.Point(12, 5);
+            this.conectare.Name = "conectare";
+            this.conectare.Size = new System.Drawing.Size(138, 31);
+            this.conectare.TabIndex = 1;
+            this.conectare.Text = "Conectare";
+            this.conectare.UseVisualStyleBackColor = true;
+            this.conectare.Click += new System.EventHandler(this.conectare_Click);
+            // 
+            // powerTextBox
+            // 
+            this.powerTextBox.Location = new System.Drawing.Point(233, 469);
+            this.powerTextBox.Name = "powerTextBox";
+            this.powerTextBox.Size = new System.Drawing.Size(136, 20);
+            this.powerTextBox.TabIndex = 5;
+            // 
+            // power
+            // 
+            this.power.AutoSize = true;
+            this.power.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.power.Location = new System.Drawing.Point(9, 471);
+            this.power.Name = "power";
+            this.power.Size = new System.Drawing.Size(58, 18);
+            this.power.TabIndex = 4;
+            this.power.Text = "Power";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(12, 562);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(138, 31);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Salveaza";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // effectTextBox
+            // 
+            this.effectTextBox.Location = new System.Drawing.Point(233, 513);
+            this.effectTextBox.Name = "effectTextBox";
+            this.effectTextBox.Size = new System.Drawing.Size(136, 20);
+            this.effectTextBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 515);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Additional Effects";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridView2.Location = new System.Drawing.Point(694, 102);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(333, 291);
+            this.dataGridView2.TabIndex = 9;
+            this.dataGridView2.SelectionChanged += new System.EventHandler(this.dataGridView2_SelectionChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Potion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(690, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Potion Type";
+            // 
+            // type
+            // 
+            this.type.AutoSize = true;
+            this.type.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.type.Location = new System.Drawing.Point(9, 428);
+            this.type.Name = "type";
+            this.type.Size = new System.Drawing.Size(28, 18);
+            this.type.TabIndex = 2;
+            this.type.Text = "Id";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Enabled = false;
+            this.idTextBox.Location = new System.Drawing.Point(233, 426);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(136, 20);
+            this.idTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 671);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.effectTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.powerTextBox);
+            this.Controls.Add(this.power);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.type);
+            this.Controls.Add(this.conectare);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button conectare;
+        private System.Windows.Forms.TextBox powerTextBox;
+        private System.Windows.Forms.Label power;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox effectTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label type;
+        private System.Windows.Forms.TextBox idTextBox;
+    }
+}
+
---
+++ app/app/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace app
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source=DESKTOP-93U1OMQ;Initial Catalog=RPG;Integrated Security=True");
+        SqlDataAdapter daParinte = new SqlDataAdapter();
+        DataSet dsParinte = new DataSet();
+        SqlDataAdapter daFiu = new SqlDataAdapter();
+        DataSet dsFiu = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+            daParinte.SelectCommand = new SqlCommand("SELECT * FROM potion_type", cs);
+            dsParinte.Clear();
+            daParinte.Fill(dsParinte);
+            dataGridView2.DataSource = dsParinte.Tables[0];
+        }
+
+        private void ResetForm()
+        {
+            idTextBox.Text = "";
+            powerTextBox.Text = "";
+            effectTextBox.Text = "";
+        }
+
+        private void ClearTable(DataSet ds, SqlDataAdapter da)
+        {
+            ds.Clear();
+            da.Fill(dsFiu);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void conectare_Click(object sender, EventArgs e)
+        {
+            //da.SelectCommand = new SqlCommand("SELECT * FROM potion", cs);
+            //ds.Clear();
+            //da.Fill(ds);
+            //dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataGridViewCell cell = dataGridView2.SelectedRows[0].Cells[0];
+                if (String.IsNullOrEmpty(idTextBox.Text))
+                {
+                    daFiu.InsertCommand = new SqlCommand("INSERT INTO potion (potion_type_int, power_effect, additional_effect) VALUES(@t, @p, @e)", cs);
+                    daFiu.InsertCommand.Parameters.Add("@t", SqlDbType.Int).Value = Int32.Parse(cell.Value.ToString());
+                    daFiu.InsertCommand.Parameters.Add("@p", SqlDbType.Int).Value = Int32.Parse(powerTextBox.Text);
+                    daFiu.InsertCommand.Parameters.Add("@e", SqlDbType.VarChar).Value = effectTextBox.Text;
+                    cs.Open();
+                    daFiu.InsertCommand.ExecuteNonQuery();
+                    MessageBox.Show("Inserted successfully into the database.");
+                    cs.Close();
+                }
+                else
+                {
+
+                    daFiu.UpdateCommand = new SqlCommand("UPDATE potion SET potion_type_int = @t, power_effect = @p, additional_effect = @e WHERE id = @i", cs);
+                    daFiu.UpdateCommand.Parameters.Add("@t", SqlDbType.Int).Value = Int32.Parse(cell.Value.ToString());
+                    daFiu.UpdateCommand.Parameters.Add("@p", SqlDbType.Int).Value = Int32.Parse(powerTextBox.Text);
+                    daFiu.UpdateCommand.Parameters.Add("@e", SqlDbType.VarChar).Value = effectTextBox.Text;
+                    daFiu.UpdateCommand.Parameters.Add("@i", SqlDbType.Int).Value = Int32.Parse(idTextBox.Text);
+                    cs.Open();
+                    daFiu.UpdateCommand.ExecuteNonQuery();
+                    MessageBox.Show("Updated successfully the database.");
+                    cs.Close();
+                }
+                // already inserted
+                ClearTable(dsFiu, daFiu);
+                ResetForm();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cs.Close();
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DataGridViewRow row = dataGridView1.SelectedRows[0];
+                idTextBox.Text = row.Cells[0].Value.ToString();
+                powerTextBox.Text = row.Cells[2].Value.ToString();
+                effectTextBox.Text = row.Cells[3].Value.ToString();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void dataGridView2_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DataGridViewCell cell = dataGridView2.SelectedRows[0].Cells[0];
+                daFiu.SelectCommand = new SqlCommand("SELECT * FROM potion where potion_type_int = @p", cs);
+                daFiu.SelectCommand.Parameters.Add("@p", Int32.Parse(cell.Value.ToString()));
+                cs.Open();
+                daFiu.SelectCommand.ExecuteNonQuery();
+                cs.Close();
+            }
+            catch(Exception ex)
+            {
+                cs.Close();
+            }
+            ClearTable(dsFiu, daFiu);
+        }
+    }
+}
